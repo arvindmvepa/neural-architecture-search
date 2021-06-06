68406ea377bc690f9059da66008da56780feabdb
arvind <arvind.m.vepa@gmail.com>
2021-06-05 19:30:50 -0700

    added densenet

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
-from tensorflow.compat.v1.keras.models import Model
+#from tensorflow.compat.v1.keras.models import Model
 from tensorflow.compat.v1.keras.layers import Input, Dense, Conv2D, GlobalAveragePooling2D
+from tensorflow.compat.v1-keras.applications import DenseNet121
 
 # generic model design
 def model_fn(actions, image_dim):
+    """
+    # comment out generally
     # unpack the actions from the list
     kernel_1, filters_1, kernel_2, filters_2, kernel_3, filters_3, kernel_4, filters_4 = actions
-
-    ip = Input(shape=image_dim)
     x = Conv2D(filters_1, (kernel_1, kernel_1), strides=(2, 2), padding='same', activation='relu')(ip)
     x = Conv2D(filters_2, (kernel_2, kernel_2), strides=(1, 1), padding='same', activation='relu')(x)
     x = Conv2D(filters_3, (kernel_3, kernel_3), strides=(2, 2), padding='same', activation='relu')(x)
@@ -16,4 +17,12 @@ def model_fn(actions, image_dim):
     #x = Dense(10, activation='softmax')(x)
 
     model = Model(ip, x)
+    """
+    ip = Input(shape=image_dim)
+    model = DenseNet121(include_top=True,
+                        weights="imagenet",
+                        input_tensor=ip,
+                        input_shape=image_dim,
+                        pooling=None,
+                        classes=2)
     return model
